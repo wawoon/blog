38a5b42df7ef0867ff2d8ae07ae92fbe9b720b43
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-06-07 00:23:30 +0900

    feature: setup mdx

---
+++ layouts/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Link from "next/link";
+import { css } from "@emotion/core";
+
+// @ts-ignore
+const filenames: string[] = preval`
+  const fs = require('fs');
+  const path = require('path');
+
+  const postsDirectory = path.join(process.cwd(), 'pages/posts')
+  const mdxFiles = fs.readdirSync(postsDirectory)
+  console.log('the queried pages', mdxFiles)
+  // Loop through all post files and create array of slugs (to create links)
+  const paths = mdxFiles.map(filename => filename.replace(".md", ""));
+  module.exports = paths;
+`;
+
+console.log(filenames);
+const Layout = (frontMatter: any) => {
+  const Component: React.FC = (props) => {
+    return (
+      <div>
+        <div
+          css={css`
+            display: flex;
+          `}
+        >
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+              width: 400px;
+              border-right: 1px solid #eee;
+            `}
+          >
+            {filenames.map((name) => {
+              return (
+                <Link href={`/posts/${name}`}>
+                  <a>{name}</a>
+                </Link>
+              );
+            })}
+          </div>
+          <div>
+            <h1>{frontMatter.title}</h1>
+            {props.children}
+          </div>
+        </div>
+      </div>
+    );
+  };
+  return Component;
+};
+
+export default Layout;
