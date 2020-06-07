06ed4ea9bc6ccbbf2ddb5ba63964d895f3d2a36e
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-06-07 09:51:11 +0900

    feature: better layout

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,24 +1,76 @@
 import React from "react";
 import Link from "next/link";
 import { css } from "@emotion/core";
+import styled from "@emotion/styled";
 
 // @ts-ignore
-const filenames: string[] = preval`
+const fileData: {
+  filename: string;
+  data: {
+    attributes: any;
+    body: string;
+    bodyBegin: number;
+    frontmatter: string;
+  };
+}[] = preval`
   const fs = require('fs');
   const path = require('path');
+  const fm = require('front-matter');
 
-  const postsDirectory = path.join(process.cwd(), 'pages/posts')
+  const postDirPath = 'pages/posts';
+  const postsDirectory = path.join(process.cwd(), postDirPath)
   const mdxFiles = fs.readdirSync(postsDirectory)
-  // console.log('the queried pages', mdxFiles)
-  // Loop through all post files and create array of slugs (to create links)
-  const paths = mdxFiles.map(filename => filename.replace(".mdx", ""));
-  module.exports = paths;
+  let list = [];
+
+  for (const filename of mdxFiles) {
+    const content = fs.readFileSync(path.join(process.cwd(), postDirPath, filename), { encoding: "utf-8" });
+    const data = fm(content);
+    const { attributes, body } = data;
+    list.push({ filename, data: { attributes, body } });
+  }
+
+  module.exports = list;
+`;
+// console.log(fileData);
+
+const HeaderLink = styled.a`
+  :not(:last-child) {
+    margin-right: 8px;
+  }
 `;
 
 const Layout = (frontMatter: any) => {
   const Component: React.FC = (props) => {
     return (
-      <div>
+      <div css={css``}>
+        <div
+          css={css`
+            height: 60px;
+            position: sticky;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            top: 0;
+            background: white;
+            z-index: 1;
+            font-weight: bold;
+          `}
+        >
+          <div>wawoon.dev</div>
+          <div
+            css={css`
+              padding: 12px;
+            `}
+          >
+            <HeaderLink href="https://twitter.com/wawoon_jp">
+              Twitter
+            </HeaderLink>
+            <HeaderLink href="https://github.com/wawoon">GitHub</HeaderLink>
+            <HeaderLink href="https://atcoder.jp/users/wawoon">
+              AtCoder
+            </HeaderLink>
+          </div>
+        </div>
         <div
           css={css`
             display: flex;
@@ -26,19 +78,39 @@ const Layout = (frontMatter: any) => {
         >
           <div
             css={css`
-              display: flex;
-              flex-direction: column;
               width: 240px;
               border-right: 1px solid #eee;
             `}
           >
-            {filenames.map((name) => {
-              return (
-                <Link href={`/posts/${name}`}>
-                  <a>{name}</a>
-                </Link>
-              );
-            })}
+            <div
+              css={css`
+                position: sticky;
+                top: 0;
+                display: flex;
+                flex-direction: column;
+                overflow-y: scroll;
+                max-height: 100vh;
+              `}
+            >
+              {fileData.map((file) => {
+                return (
+                  <Link href={`/posts/${file.filename.replace(".mdx", "")}`}>
+                    <a>
+                      <div
+                        css={css`
+                          display: flex;
+                          padding: 8px;
+                          border-bottom: 1px solid #eee;
+                          font-size: 14px;
+                        `}
+                      >
+                        {file.data.attributes.title}
+                      </div>
+                    </a>
+                  </Link>
+                );
+              })}
+            </div>
           </div>
           <div
             css={css`
