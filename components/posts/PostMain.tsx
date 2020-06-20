06c1bbf18b017dcf24124185a45fdacf81e31ac2
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-06-20 20:28:42 +0900

    fix

--- components/posts/PostMain.tsx
+++ components/posts/PostMain.tsx
@@ -4,6 +4,7 @@ import { dateToString } from "../../lib/date";
 import { PostTagContainer, PostTag } from "../../components/PostTag";
 
 export const PostHeader = ({ frontMatter }: { frontMatter: FrontMatter }) => {
+  console.log("post_header", frontMatter);
   return (
     <div>
       <h1
