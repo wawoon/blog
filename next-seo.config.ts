f32c9315d19515ada4ce9e4611e39f80518c7060
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-06-21 18:02:25 +0900

    feature: add twitter ogp

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -2,6 +2,10 @@ import NextSEO from "next-seo";
 
 export const config: NextSEO.NextSeoProps = {
   titleTemplate: "%s | wawoon.dev",
+  twitter: {
+    handle: "@wawoon_jp",
+    site: "@wawoon_jp",
+  },
 };
 
 export default config;
