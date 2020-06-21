973de68dbfae516edd7c78688739af124fdd6a5e
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-06-21 19:13:45 +0900

    refactor: remove sidebar

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,6 @@ import Link from "next/link";
 import { css } from "@emotion/core";
 import styled from "@emotion/styled";
 import tw from "tailwind.macro";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
-import { useAmp } from "next/amp";
 
 const HeaderLink = styled.a`
   ${tw`text-red-600`}
@@ -13,75 +11,67 @@ const HeaderLink = styled.a`
   }
 `;
 
-interface Props {
-  sidebarOpen: boolean;
-  sidebarToggle: () => void;
-}
+const headerHeight = 48;
 
-export const Header = (props: Props) => {
-  const isAmp = useAmp();
-
+export const Header = () => {
   return (
-    <div
-      css={css`
-        height: 48px;
-        position: sticky;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        top: 0;
-        background: white;
-        z-index: 1;
-        font-weight: bold;
-        border-bottom: 2px solid #eee;
-      `}
-    >
+    <>
       <div
         css={css`
+          height: ${headerHeight}px;
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
           display: flex;
           align-items: center;
+          justify-content: space-between;
+          background: white;
+          z-index: 1;
+          font-weight: bold;
+          border-bottom: 2px solid #eee;
         `}
       >
-        {!isAmp && (
-          <div
-            css={css`
-              ${tw`hidden md:flex`}
-            `}
-            onClick={props.sidebarToggle}
-          >
-            {props.sidebarOpen ? (
-              <FaAngleLeft size={20} />
-            ) : (
-              <FaAngleRight size={20} />
-            )}
-          </div>
-        )}
         <div
           css={css`
-            font-weight: bold;
+            display: flex;
+            align-items: center;
+          `}
+        >
+          <div
+            css={css`
+              font-weight: bold;
+              padding: 12px;
+            `}
+          >
+            <Link href={"/"} passHref>
+              <a
+                css={css`
+                  text-decoration: none;
+                `}
+              >
+                wawoon.dev
+              </a>
+            </Link>
+          </div>
+        </div>
+        <div
+          css={css`
             padding: 12px;
           `}
         >
-          <Link href={"/"} passHref>
-            <a
-              css={css`
-                text-decoration: none;
-              `}
-            >
-              wawoon.dev
-            </a>
-          </Link>
+          <HeaderLink href="https://twitter.com/wawoon_jp">Twitter</HeaderLink>
+          <HeaderLink href="https://github.com/wawoon">GitHub</HeaderLink>
+          <HeaderLink href="https://atcoder.jp/users/wawoon">
+            AtCoder
+          </HeaderLink>
         </div>
       </div>
       <div
         css={css`
-          padding: 12px;
+          height: ${headerHeight}px;
         `}
-      >
-        <HeaderLink href="https://twitter.com/wawoon_jp">Twitter</HeaderLink>
-        <HeaderLink href="https://github.com/wawoon">GitHub</HeaderLink>
-        <HeaderLink href="https://atcoder.jp/users/wawoon">AtCoder</HeaderLink>
-      </div>
-    </div>
+      />
+    </>
   );
 };
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { css } from "@emotion/core";
 import tw from "tailwind.macro";
-import fileData from "../post_data";
 import { Header } from "./Header";
-import { SideBarPostCard } from "./SideBarPostCard";
-import { useWindowSize } from "../hooks/useWIndowSize";
 
 export const Layout: React.FC = (props) => {
-  const [sidebarOpen, setSidebarOpen] = React.useState(false);
-  const size = useWindowSize();
-  const showSidebar = sidebarOpen || (size.width && size.width < 768);
-
   return (
     <div
       css={css`
@@ -19,45 +12,16 @@ export const Layout: React.FC = (props) => {
     >
       <div
         css={css`
-          ${tw`flex flex-col-reverse md:flex-row`}
+          ${tw`flex`}
         `}
       >
-        <div
-          css={css`
-            background: #fff;
-            transition: 0.3s;
-            ${tw`w-full`}
-            ${tw`md:w-1/4 lg:w-1/5`}
-          `}
-          style={showSidebar ? {} : { width: 0 }}
-        >
-          <div
-            css={css`
-              position: sticky;
-              top: 0;
-              display: flex;
-              flex-direction: column;
-              overflow-y: scroll;
-              max-height: 100vh;
-              box-shadow: 0 2px 5px rgba(0, 0, 0, 0.26);
-            `}
-          >
-            {fileData.map((file) => {
-              const slug = file.filename.replace(".mdx", "");
-              return <SideBarPostCard key={slug} file={file} />;
-            })}
-          </div>
-        </div>
         <div
           css={css`
             flex: 1;
             overflow: auto;
           `}
         >
-          <Header
-            sidebarOpen={sidebarOpen}
-            sidebarToggle={() => setSidebarOpen(!sidebarOpen)}
-          />
+          <Header />
           <div
             css={css`
               word-break: break-word;
