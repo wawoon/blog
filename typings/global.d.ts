9c17c0996ddbc0a2e3d644a0d6af679bdf267012
Yoshinori Kosaka <yoshinori.ksk@gmail.com>
2025-08-05 10:46:05 +0900

    chore(typings): extend global Window and globalThis.Prism typings to remove any casts in CodeBlock

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -8,3 +8,21 @@ type FrontMatter = {
   image?: string;
   __resourcePath: string;
 };
+
+/**
+ * Extend the Window interface so that we don’t have to cast to `any` when
+ * assigning `Prism` for syntax-highlighting at runtime.
+ *
+ * NOTE: The import is `type`-only so it will be removed from the emitted
+ * JavaScript bundle and therefore has no impact on client size.
+ */
+import type PrismType from "prism-react-renderer/prism";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var Prism: PrismType | undefined;
+  interface Window {
+    Prism: PrismType;
+  }
+}
+
